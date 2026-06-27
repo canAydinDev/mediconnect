5ef42ce22d255dc10a6ecbd52964832bc07728ff
can aydın <can@cans-MacBook-Pro.local>
2026-06-27 14:32:51 +0200

    update contant-message

--- src/main/java/com/canaydin/mediconnect/contact/service/impl/ContactMessageServiceImpl.java
+++ src/main/java/com/canaydin/mediconnect/contact/service/impl/ContactMessageServiceImpl.java
@@ -5,8 +5,11 @@ import com.canaydin.mediconnect.contact.dto.ContactMessageResponse;
 import com.canaydin.mediconnect.contact.entity.ContactMessage;
 import com.canaydin.mediconnect.contact.repository.ContactMessageRepository;
 import com.canaydin.mediconnect.contact.service.ContactMessageService;
+import com.canaydin.mediconnect.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +17,18 @@ public class ContactMessageServiceImpl implements ContactMessageService {
 
     private final ContactMessageRepository contactMessageRepository;
 
+
+    @Override
+    public ContactMessageResponse findContactMessageById(Long id) {
+        ContactMessage contactMessage = contactMessageRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Contact Message", "id", id));
+        return transformContactMessage(contactMessage);
+    }
+
+    @Override
+    public List<ContactMessageResponse> findContactMessageByStatus(String status) {
+        return contactMessageRepository.findByStatus(status).
+                stream().map(this::transformContactMessage).toList();
+    }
 
     @Override
     public ContactMessageResponse createContactMessage(ContactMessageRequest contactMessageRequest) {
@@ -27,6 +42,29 @@ public class ContactMessageServiceImpl implements ContactMessageService {
         ContactMessage savedContactMessage = contactMessageRepository.save(contactMessage);
 
         return transformContactMessage(savedContactMessage);
+    }
+
+    @Override
+    public List<ContactMessageResponse> findAllContactMessages() {
+        List<ContactMessage> allContactMessages = contactMessageRepository.findAll();
+       return allContactMessages.stream().map(this::transformContactMessage).toList();
+    }
+
+
+    @Override
+    public void deleteContactMessage(Long id) {
+        ContactMessage contactMessage = contactMessageRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Contact Message", "id", id));
+        contactMessageRepository.delete(contactMessage);
+
+    }
+
+    @Override
+    public ContactMessageResponse updateStatus(Long id, String status) {
+        ContactMessage contactMessage = contactMessageRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Contact Message", "id", id));
+        contactMessage.setStatus(status);
+        contactMessageRepository.save(contactMessage);
+        return transformContactMessage(contactMessage);
+
     }
 
     private ContactMessageResponse transformContactMessage(ContactMessage contactMessage) {
